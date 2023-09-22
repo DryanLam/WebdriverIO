05f61cdfe2259e56f0d8ea3e81681d4f5a2e692b
Dryan <dunglam@kms-technology.com>
2023-09-22 11:54:55 +0700

    Debug Mocha OK However, run all "it" block at current open file

---
+++ tests/hello.spec.ts
@@ -0,0 +1,11 @@
+describe('Hello', () => {
+    it('Test Hello 1', () => {
+        let val = console.log("hello mocha 1 ");
+    });
+
+
+    it('Test Hello 2', () => {
+        let val = console.log("hello mocha 2");
+    });
+    
+})
