dfb30db478d13382be1538bd9a49a466e56cfca4
Dryan <dunglam@kms-technology.com>
2023-09-22 12:28:09 +0700

    Mocha Test Explorer

--- .mocharc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    spec: "tests/**/*.ts", // Specify the test files pattern.
-    require: ["ts-node/register"], // Use if you need to transpile your code (e.g., with Babel).
-    reporter: "spec", // You can choose different reporters (e.g., "nyan", "dot", etc.).
-};
